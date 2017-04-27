279e8c4d956dd7fc8049beeb760d8654d1996d83
levsthings <bearnode@gmail.com>
2017-04-27 15:50:56 +0300

    add official examples

--- topics/05_interfaces/01_basics.ts
+++ topics/05_interfaces/01_basics.ts
@@ -22,3 +22,22 @@ interface PersonWithAName {
 }
 
 // to be continued ..
+
+interface SquareConfig {
+    color?: string;
+    width?: number;
+}
+
+function createSquare(config: SquareConfig): { color: string; area: number } {
+    let newSquare = {color: "white", area: 100};
+    if (config.color) {
+        // Error: Property 'clor' does not exist on type 'SquareConfig'
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+
+let mySquare = createSquare({color: "black"});
